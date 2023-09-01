6d56b20478531aba0c04c1ffe2ceee24dd4c526e
joaoxmarcello <104221541+joaoxmarcello@users.noreply.github.com>
2023-09-01 15:24:52 -0300

    addition javascript part 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativa os Links do Menu para indicar qual pagina está sendo acessada.
 const links = document.querySelectorAll(".header-menu a");
 function ativarLink (link) {
   const url = window.location.href;
@@ -7,3 +8,14 @@ function ativarLink (link) {
   } 
 }
 links.forEach(ativarLink)
+
+// Ativa os itens do orçamento
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+  elemento.checked = true;
+  }
+  console.log(elemento);
+}
+parametros.forEach(ativarProduto)

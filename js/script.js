5fa14b9144674109be6b7ef5b678a49466d212f1
joaoxmarcello <104221541+joaoxmarcello@users.noreply.github.com>
2023-09-02 10:29:58 -0300

    swap function in image gallery

--- js/script.js
+++ js/script.js
@@ -36,4 +36,23 @@ function eventosPerguntas(pergunta) {
   pergunta.addEventListener("click", ativarPergunta);
 }
 
-perguntas.forEach(eventosPerguntas)
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de imagens
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = window.matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria)
